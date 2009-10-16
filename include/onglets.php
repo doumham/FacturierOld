89901ef7872b01d782602fbf128c73678bec3d6b
De Backer <sdebacker@gmail.com>
2009-10-16 19:48:51 +0200

    working on design

--- include/onglets.php
+++ include/onglets.php
@@ -6,8 +6,8 @@ function checkActif($page){
 }
 ?>
 		<ul id="onglets">
-			<li><a <?php checkActif('facturesSortantes.php'); ?> href="facturesSortantes.php?annee=<?php echo $annee ?>">Factures sortantes</a></li>
-			<li><a <?php checkActif('facturesEntrantes.php'); ?> href="facturesEntrantes.php?annee=<?php echo $annee ?>">Factures entrantes</a></li>
-			<li><a <?php checkActif('statistiques.php'); ?> href="statistiques.php?annee=<?php echo $annee ?>">Statistiques</a></li>
-			<li><a <?php checkActif('stats.php'); ?> href="stats.php?annee=<?php echo $annee ?>" title="Graphique">Graphique</a></li>
+			<li <?php checkActif('facturesSortantes.php'); ?>><a href="facturesSortantes.php?annee=<?php echo $annee ?>">Factures sortantes</a></li>
+			<li <?php checkActif('facturesEntrantes.php'); ?>><a href="facturesEntrantes.php?annee=<?php echo $annee ?>">Factures entrantes</a></li>
+			<li <?php checkActif('statistiques.php'); ?>><a href="statistiques.php?annee=<?php echo $annee ?>">Statistiques</a></li>
+			<li <?php checkActif('stats.php'); ?>><a href="stats.php?annee=<?php echo $annee ?>" title="Graphique">Graphique</a></li>
 		</ul>
