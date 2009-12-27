b9fe030bd5e22584ff1270e814dfe4019c086f41
De Backer <sdebacker@gmail.com>
2009-12-27 19:06:31 +0100

    small bug

--- formClient.php
+++ formClient.php
@@ -1,5 +1,4 @@
 <?php
-date_default_timezone_set('Europe/Brussels');
 include ('classes/interface.class.php');
 $myInterface = new interface_();
 $id = '';
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php 
-date_default_timezone_set('Europe/Brussels');
 header("location:factures.php?type=sortantes&annee=".date('Y')."#bottom");
 ?>
