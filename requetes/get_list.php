c917e296321d5a972dc79b7b6741181a9de4349e
Samuel De Backer <sdebacker@gmail.com>
2011-12-26 18:35:31 +0100

    htmlspecialchars

--- requetes/get_list.php
+++ requetes/get_list.php
@@ -4,7 +4,7 @@ $table = $_GET['table'];
 $champ = $_GET['champ'];
 $select = mysql_query("SELECT DISTINCT `".$champ."` FROM `".$table."`") or trigger_error(mysql_error(),E_USER_ERROR);
 while($f = mysql_fetch_array($select)){
-	$return[] = $f[$champ];
+	$return[] = htmlspecialchars($f[$champ]);
 }
 echo '["'.implode('","',$return).'"]';
 ?>
